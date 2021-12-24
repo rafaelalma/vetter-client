ce07395f66a22be360483850664a47aabdfc18b4
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-24 16:43:28 +0100

    Add stringFormatting capitalize helper function

---
+++ src/utils/helpers/stringFormatting.test.js
@@ -0,0 +1,21 @@
+import { capitalize } from "./stringFormatting";
+
+describe("capitalize", () => {
+  it("should capitalize all lowercase string", () => {
+    expect(capitalize("testing capitalize function")).toBe(
+      "Testing capitalize function"
+    );
+  });
+
+  it("should capitalize all uppercase string", () => {
+    expect(capitalize("TESTING CAPITALIZE FUNCTION")).toBe(
+      "Testing capitalize function"
+    );
+  });
+
+  it("should capitalize random cased string", () => {
+    expect(capitalize("testING CaPiTaLiZe FUNCtion")).toBe(
+      "Testing capitalize function"
+    );
+  });
+});
