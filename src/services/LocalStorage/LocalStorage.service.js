db47bd5bd7e4e4490ed7044de153bdc8a7fda25c
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-24 16:17:38 +0100

    Add LocalStorage service

---
+++ src/services/LocalStorage/LocalStorage.service.js
@@ -0,0 +1,17 @@
+export const LocalStorage = {
+  set(key, value) {
+    localStorage.setItem(key, value);
+  },
+
+  get(key) {
+    return localStorage.getItem(key);
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  },
+
+  clear() {
+    localStorage.clear();
+  },
+};
---
+++ src/services/LocalStorage/LocalStorage.test.js
@@ -0,0 +1,22 @@
+import { LocalStorage } from "./LocalStorage.service";
+
+describe("LocalStorage", () => {
+  it("should set values by key", () => {
+    LocalStorage.set("key", "value");
+    expect(localStorage.getItem("key")).toBe("value");
+  });
+
+  it("should get values by key", () => {
+    localStorage.setItem("key", "value");
+    expect(LocalStorage.get("key")).toBe("value");
+  });
+
+  it("should remove values by key", () => {
+    localStorage.setItem("key", "value");
+    expect(localStorage.getItem("key")).toBe("value");
+    LocalStorage.remove("key");
+    expect(localStorage.getItem("key")).toBeNull();
+  });
+
+  xit("should clear values", () => {});
+});
