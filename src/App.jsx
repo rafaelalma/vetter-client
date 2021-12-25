35ea4430a4f1f8c7d606bf7f27ff2d97b4e8d3ae
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-25 23:01:50 +0100

    Implement basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import { AuthenticationProvider } from "./context/authentication/AuthenticationContext";
+import SignUpPage from "./views/SignUp/SignUpPage";
 
 export default function App() {
   return (
     <div className="App">
-      <AuthenticationProvider>
-        <Header />
-      </AuthenticationProvider>
+      <BrowserRouter>
+        <AuthenticationProvider>
+          <Header />
+        </AuthenticationProvider>
+        <Routes>
+          <Route path="/" element={null} />
+          <Route path="/login" element={null} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
