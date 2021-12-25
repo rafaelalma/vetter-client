05391d1697ab022298ecc45bb3ba6118d887e931
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-26 00:16:50 +0100

    Add LoginForm component

---
+++ src/views/Login/LoginForm/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../../components/buttons";
+import { Input } from "../../../components/forms";
+import { AuthenticationContext } from "../../../context/authentication/AuthenticationContext";
+import "./LoginForm.scss";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(AuthenticationContext);
+
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: backend
+
+    context.authenticate();
+
+    navigate("/dashboard");
+  };
+
+  return (
+    <form className="Login" onSubmit={handleLoginSubmit}>
+      <Input
+        placeholder="Email"
+        required
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        required
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <Button type="submit">Login</Button>
+    </form>
+  );
+}
---
+++ src/views/Login/LoginForm/LoginForm.test.js
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import LoginForm from "./LoginForm";
+
+describe("LoginForm", () => {
+  it("should render Email type input", () => {
+    render(<LoginForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Email*");
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("email");
+  });
+
+  it("should update email value with user input", () => {
+    render(<LoginForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Email*");
+    fireEvent.change(inputElement, { target: { value: "test@email.com" } });
+    expect(inputElement.value).toBe("test@email.com");
+  });
+
+  it("should render Password type input", () => {
+    render(<LoginForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Password*");
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("password");
+  });
+
+  it("should update password value with user input", () => {
+    render(<LoginForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Password*");
+    fireEvent.change(inputElement, { target: { value: "123456Ab#@!" } });
+    expect(inputElement.value).toBe("123456Ab#@!");
+  });
+
+  it("should render Login submit type button", () => {
+    render(<LoginForm />, { wrapper: BrowserRouter });
+    const buttonElement = screen.getByRole("button", { name: "Login" });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement.type).toBe("submit");
+  });
+
+  xit("should submit Login form", () => {
+    // TODO
+  });
+
+  xit("should authenticate if valid user", () => {
+    // TODO
+  });
+
+  xit("should navigate to Dashboard if valid user", () => {
+    // TODO
+  });
+});
