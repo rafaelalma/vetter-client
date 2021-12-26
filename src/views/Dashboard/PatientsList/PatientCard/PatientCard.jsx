cebe231d5e55cb72693128275caf82d5345fa506
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-26 23:02:06 +0100

    Refactor PatientCard component

--- src/views/Dashboard/PatientsList/PatientCard/PatientCard.jsx
+++ src/views/Dashboard/PatientsList/PatientCard/PatientCard.jsx
@@ -4,10 +4,10 @@ export default function PatientCard({ patient }) {
   const { name, species, breed } = patient;
 
   return (
-    <div className="PatientCard">
+    <li className="PatientCard">
       <h3>{name}</h3>
       <p>{species}</p>
       <p>{breed}</p>
-    </div>
+    </li>
   );
 }
