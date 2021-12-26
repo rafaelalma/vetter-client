4ad451f9fb19db7393c570eb9dea799c48ae944e
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-26 17:27:34 +0100

    Add dummy PatientsList component

---
+++ src/views/Dashboard/PatientsList/PatientsList.jsx
@@ -0,0 +1,12 @@
+import "./PatientsList.scss";
+
+export default function PatientsList() {
+  return (
+    <ul className="PatientsList">
+      <li>Patient 1</li>
+      <li>Patient 2</li>
+      <li>Patient 3</li>
+      <li>Patient 4</li>
+    </ul>
+  );
+}
