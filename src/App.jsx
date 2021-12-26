0f686d1c543b0530822dcc87db55d2a4f6daa4de
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-26 17:30:48 +0100

    Implement Dashboard routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import { AuthenticationProvider } from "./context/authentication/AuthenticationContext";
+import DashboardPage from "./views/Dashboard/DashboardPage";
+import PatientsList from "./views/Dashboard/PatientsList/PatientsList";
 import ForgotPasswordPage from "./views/ForgotPassword/ForgotPasswordPage";
 import LoginPage from "./views/Login/LoginPage";
 import SignUpPage from "./views/SignUp/SignUpPage";
@@ -18,7 +20,11 @@ export default function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
-            <Route path="/dashboard" element={null} />
+            <Route path="/dashboard" element={<DashboardPage />}>
+              <Route index element={null} />
+              <Route path="patients" element={<PatientsList />} />
+              <Route path="saved-medicines" element={null} />
+            </Route>
             <Route path="/medicines" element={null} />
           </Routes>
         </BrowserRouter>
