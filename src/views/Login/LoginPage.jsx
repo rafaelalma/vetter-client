8bc2084598c47b625907598c81ce2c3ef39da644
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-26 01:18:57 +0100

    Add forgot password link to LoginPage

--- src/views/Login/LoginPage.jsx
+++ src/views/Login/LoginPage.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from "react-router-dom";
 import LoginForm from "./LoginForm/LoginForm";
+import "./LoginPage.scss";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+      {/* TODO: component */}
+      <div className="ForgotPasswordLink">
+        <NavLink className="link-accent" to="/forgot-password">
+          Forgot your password?
+        </NavLink>
+      </div>
+    </>
+  );
 }
