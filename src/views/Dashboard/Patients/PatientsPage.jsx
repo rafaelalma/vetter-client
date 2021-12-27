079953bd8f40de2bebfcea2e4aa76baaf3f2e9a7
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-27 16:06:48 +0100

    Refactor PatientsPage component

--- src/views/Dashboard/Patients/PatientsPage.jsx
+++ src/views/Dashboard/Patients/PatientsPage.jsx
@@ -1,5 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "./PatientsPage.scss";
 
 export default function PatientsPage() {
-  return <Outlet />;
+  return (
+    <>
+      <NavLink to="new" className="link-primary patients-page-new-patient-link">
+        New Patient
+      </NavLink>
+      <Outlet />
+    </>
+  );
 }
