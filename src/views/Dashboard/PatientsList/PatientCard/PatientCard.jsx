cc87bc067b64b19ffc9d40a10dd7dfd807729d68
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-27 12:25:02 +0100

    Refactor PatientCard component

--- src/views/Dashboard/PatientsList/PatientCard/PatientCard.jsx
+++ src/views/Dashboard/PatientsList/PatientCard/PatientCard.jsx
@@ -1,20 +1,33 @@
 import propTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./PatientCard.scss";
 
 export default function PatientCard({ patient }) {
-  const { name, species, breed } = patient;
+  const { id, name, species, breed } = patient;
 
   return (
     <li className="PatientCard">
-      <h3>{name}</h3>
-      <p>{species}</p>
-      <p>{breed}</p>
+      <Link to={`/dashboard/patients/${id}`}>
+        <img
+          src=""
+          alt=""
+          width="100"
+          height="100"
+          className="patient-card-img"
+        />
+      </Link>
+      <div>
+        <h3>{name}</h3>
+        <p>{species}</p>
+        <p>{breed}</p>
+      </div>
     </li>
   );
 }
 
 PatientCard.propTypes = {
   patient: propTypes.shape({
+    id: propTypes.number.isRequired,
     name: propTypes.string.isRequired,
     species: propTypes.string.isRequired,
     breed: propTypes.string.isRequired,
