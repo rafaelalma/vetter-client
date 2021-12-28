726c7a16e196dca2d05eb281fc3fdc9edef25459
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-28 01:49:02 +0100

    Add useFirstTimeGetSet hook

---
+++ src/hooks/useFirstTimeGetSet.hook.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+
+export default function useFirstTimeGetSet(
+  get,
+  url,
+  setState,
+  setLoading,
+  message = "get"
+) {
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    (async () => {
+      try {
+        setLoading(true);
+
+        const data = await get(url, abortController.signal);
+        console.log(message, data);
+
+        setState(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+
+        if (error.name !== "AbortError") {
+          setLoading(false);
+        }
+      }
+    })();
+
+    return () => abortController.abort();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
