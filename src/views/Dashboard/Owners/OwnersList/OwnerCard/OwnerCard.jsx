1d6fd0318b22298b1963a165e548e8018f265a3d
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-28 11:22:33 +0100

    Add OwnerCard component

---
+++ src/views/Dashboard/Owners/OwnersList/OwnerCard/OwnerCard.jsx
@@ -0,0 +1,34 @@
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./OwnerCard.scss";
+
+export default function OwnerCard({ owner }) {
+  const { id, first_name, last_name } = owner;
+
+  return (
+    <li className="OwnerCard">
+      <Link to={`/dashboard/owners/${id}`}>
+        <img
+          src=""
+          alt=""
+          width="100"
+          height="100"
+          className="owner-card-img"
+        />
+      </Link>
+      <div>
+        <h3>
+          {first_name} {last_name}
+        </h3>
+      </div>
+    </li>
+  );
+}
+
+OwnerCard.propTypes = {
+  owner: propTypes.shape({
+    id: propTypes.number.isRequired,
+    first_name: propTypes.string.isRequired,
+    last_name: propTypes.string.isRequired,
+  }),
+};
