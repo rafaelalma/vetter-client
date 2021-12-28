8203994a8a8b012228b5122c11d2fef72e7776e5
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-29 00:07:38 +0100

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
+import Header from "components/Header/Header";
+import { AuthenticationProvider } from "context/authentication/AuthenticationContext";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardPage from "views/Dashboard/DashboardPage";
+import NewOwnerForm from "views/Dashboard/Owners/NewOwnerForm/NewOwnerForm";
+import OwnerDetails from "views/Dashboard/Owners/OwnerDetails/OwnerDetails";
+import OwnersList from "views/Dashboard/Owners/OwnersList/OwnersList";
+import OwnersPage from "views/Dashboard/Owners/OwnersPage";
+import NewPatientForm from "views/Dashboard/Patients/NewPatientForm/NewPatientForm";
+import PatientDetails from "views/Dashboard/Patients/PatientDetails/PatientDetails";
+import PatientsList from "views/Dashboard/Patients/PatientsList/PatientsList";
+import PatientsPage from "views/Dashboard/Patients/PatientsPage";
+import ForgotPasswordPage from "views/ForgotPassword/ForgotPasswordPage";
+import LoginPage from "views/Login/LoginPage";
+import MedicinesPage from "views/Medicines/MedicinesPage";
+import SearchForm from "views/Medicines/SearchForm/SearchForm";
+import SignUpPage from "views/SignUp/SignUpPage";
 import "./App.scss";
-import Header from "./components/Header/Header";
-import { AuthenticationProvider } from "./context/authentication/AuthenticationContext";
-import DashboardPage from "./views/Dashboard/DashboardPage";
-import OwnerDetails from "./views/Dashboard/Owners/OwnerDetails/OwnerDetails";
-import OwnersList from "./views/Dashboard/Owners/OwnersList/OwnersList";
-import OwnersPage from "./views/Dashboard/Owners/OwnersPage";
-import NewPatientForm from "./views/Dashboard/Patients/NewPatientForm/NewPatientForm";
-import PatientDetails from "./views/Dashboard/Patients/PatientDetails/PatientDetails";
-import PatientsList from "./views/Dashboard/Patients/PatientsList/PatientsList";
-import PatientsPage from "./views/Dashboard/Patients/PatientsPage";
-import ForgotPasswordPage from "./views/ForgotPassword/ForgotPasswordPage";
-import LoginPage from "./views/Login/LoginPage";
-import MedicinesPage from "./views/Medicines/MedicinesPage";
-import SearchForm from "./views/Medicines/SearchForm/SearchForm";
-import SignUpPage from "./views/SignUp/SignUpPage";
 
 export default function App() {
   return (
@@ -37,7 +38,7 @@ export default function App() {
               </Route>
               <Route path="owners" element={<OwnersPage />}>
                 <Route index element={<OwnersList />} />
-                <Route path="new" element={null} />
+                <Route path="new" element={<NewOwnerForm />} />
                 <Route path=":id" element={<OwnerDetails />} />
               </Route>
             </Route>
