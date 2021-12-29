cd18fab913a3223f4582f4a26ab7c3ecaa66aa81
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-29 17:02:58 +0100

    Refactor SignUpForm component

--- src/views/SignUp/SignUpForm/SignUpForm.jsx
+++ src/views/SignUp/SignUpForm/SignUpForm.jsx
@@ -1,18 +1,26 @@
 import { Button } from "components/buttons";
 import { Input } from "components/forms";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./SignUpForm.scss";
 
 export default function SignUpForm() {
   const [firstName, setFirsName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const navigate = useNavigate();
+
   const handleSignUpSubmit = (event) => {
     event.preventDefault();
+
     // TODO
+
+    navigate("/login");
   };
 
   return (
@@ -30,23 +38,36 @@ export default function SignUpForm() {
         onChange={(event) => setLastName(event.target.value)}
       />
       <Input
-        placeholder="Email"
+        placeholder="Address"
         required
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
+      />
+      <Input
+        placeholder="Phone"
+        type="tel"
+        required
+        value={phone}
+        onChange={(event) => setPhone(event.target.value)}
+      />
+      <Input
+        placeholder="Email"
         type="email"
+        required
         value={email}
         onChange={(event) => setEmail(event.target.value)}
       />
       <Input
         placeholder="Password"
-        required
         type="password"
+        required
         value={password}
         onChange={(event) => setPassword(event.target.value)}
       />
       <Input
         placeholder="Confirm Password"
-        required
         type="password"
+        required
         value={confirmPassword}
         onChange={(event) => setConfirmPassword(event.target.value)}
       />
--- src/views/SignUp/SignUpForm/SignUpForm.test.js
+++ src/views/SignUp/SignUpForm/SignUpForm.test.js
@@ -1,83 +1,125 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
 
 describe("SignUpForm", () => {
-  it("should render First Name input", () => {
-    render(<SignUpForm />);
+  it("should render required First Name text type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("First Name*");
     expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("text");
+    expect(inputElement.required).toBe(true);
   });
 
   it("should update first name value with user input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("First Name*");
     fireEvent.change(inputElement, { target: { value: "first name" } });
     expect(inputElement.value).toBe("first name");
   });
 
-  it("should render Last Name input", () => {
-    render(<SignUpForm />);
+  it("should render required Last Name text type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Last Name*");
     expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("text");
+    expect(inputElement.required).toBe(true);
   });
 
   it("should update last name value with user input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Last Name*");
     fireEvent.change(inputElement, { target: { value: "last name" } });
     expect(inputElement.value).toBe("last name");
   });
 
-  it("should render Email type input", () => {
-    render(<SignUpForm />);
+  it("should render required Address text type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Address*");
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("text");
+    expect(inputElement.required).toBe(true);
+  });
+
+  it("should update address value with user input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Address*");
+    fireEvent.change(inputElement, { target: { value: "c/ Victoria, 7" } });
+    expect(inputElement.value).toBe("c/ Victoria, 7");
+  });
+
+  it("should render required Phone tel type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Phone*");
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.type).toBe("tel");
+    expect(inputElement.required).toBe(true);
+  });
+
+  it("should update phone value with user input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
+    const inputElement = screen.getByLabelText("Phone*");
+    fireEvent.change(inputElement, { target: { value: "639774728" } });
+    expect(inputElement.value).toBe("639774728");
+  });
+
+  it("should render required Email type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Email*");
     expect(inputElement).toBeInTheDocument();
     expect(inputElement.type).toBe("email");
+    expect(inputElement.required).toBe(true);
   });
 
   it("should update email value with user input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Email*");
     fireEvent.change(inputElement, { target: { value: "test@email.com" } });
     expect(inputElement.value).toBe("test@email.com");
   });
 
-  it("should render Password type input", () => {
-    render(<SignUpForm />);
+  it("should render required Password type input", () => {
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Password*");
     expect(inputElement).toBeInTheDocument();
     expect(inputElement.type).toBe("password");
+    expect(inputElement.required).toBe(true);
   });
 
   it("should update password value with user input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Password*");
     fireEvent.change(inputElement, { target: { value: "123456Ab#@!" } });
     expect(inputElement.value).toBe("123456Ab#@!");
   });
 
   it("should render Confirm Password type input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Confirm Password*");
     expect(inputElement).toBeInTheDocument();
     expect(inputElement.type).toBe("password");
+    expect(inputElement.required).toBe(true);
   });
 
   it("should update confirm password value with user input", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const inputElement = screen.getByLabelText("Confirm Password*");
     fireEvent.change(inputElement, { target: { value: "123456Ab#@!" } });
     expect(inputElement.value).toBe("123456Ab#@!");
   });
 
   it("should render Sign Up submit type button", () => {
-    render(<SignUpForm />);
+    render(<SignUpForm />, { wrapper: BrowserRouter });
     const buttonElement = screen.getByRole("button", { name: "Sign Up" });
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement.type).toBe("submit");
   });
 
   xit("should submit Sign Up form", () => {
+    // TODO
+  });
+
+  xit("should navigate to Login after submitted", () => {
     // TODO
   });
 });
