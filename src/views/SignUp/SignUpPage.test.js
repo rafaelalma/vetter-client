5f8b6bbb7774481a7bb6ae4beb051d4ca8fb5364
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-30 11:24:33 +0100

    Test SignUpPage component

---
+++ src/views/SignUp/SignUpPage.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import SignUpPage from "./SignUpPage";
+
+describe("SignUpPage", () => {
+  it("should render Sign Up form", () => {
+    render(<SignUpPage />, { wrapper: BrowserRouter });
+    const formElement = screen.getByRole("form", { name: "Sign Up" });
+    expect(formElement).toBeInTheDocument();
+    expect(formElement.classList.contains("SignUpForm")).toBe(true);
+  });
+});
